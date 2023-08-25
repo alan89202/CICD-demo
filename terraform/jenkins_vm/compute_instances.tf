8c4090078d0578144ae9787165ccd8cb91d62f61
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-25 10:31:10 +0200

    Update compute_instances.tf

--- terraform/jenkins_vm/compute_instances.tf
+++ terraform/jenkins_vm/compute_instances.tf
@@ -1,3 +1,8 @@
+data "google_compute_image" "latest_image" {
+  family  = var.family_image
+  project = var.project_image  
+}
+
 resource "google_compute_instance" "jenkins_agent_node" {
   service_account {
     email  = "terraform-service-account@alvaro-demo-elastic-000001.iam.gserviceaccount.com"
@@ -5,14 +10,14 @@ resource "google_compute_instance" "jenkins_agent_node" {
   }
   count        = var.jenkins_node
   name         = "jenkins-node-${count.index}"
-  machine_type = var.master_machine_type
+  machine_type = var.jenkins_agent_machine_type
   tags = var.jenkins_tags 
   allow_stopping_for_update = true
   zone = var.zone[count.index]
 
   boot_disk {
     initialize_params {
-      image = var.image
+      image = data.google_compute_image.latest_image.self_link
       size  = 10
       type  = "pd-standard"
     }
