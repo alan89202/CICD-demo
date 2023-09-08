4ffca50fc5652d03d8182b087b226e01e7189094
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-08 19:41:42 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,18 @@ resource "aws_security_group" "vprofile-APP-SG" {
     protocol    = "tcp"
     security_groups = [aws_security_group.vprofile-ELB-SG.id]  
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.my_public_ip]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.my_public_ip]
+  }
 }
 resource "aws_security_group" "vprofile-BACKEND-SG" {
   name        = var.backend_sg
@@ -66,9 +78,15 @@ resource "aws_security_group" "vprofile-BACKEND-SG" {
     security_groups = [aws_security_group.vprofile-APP-SG.id]  
   }
   ingress {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "tcp"
-      self        = true  
-    }
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true  
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.my_public_ip]
+  }
 }
