2992c992fd5be5ce3915392dea924d3ad60b904d
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-08 18:04:52 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,18 @@
-resource "aws_s3_bucket_acl" "vprofile_bucket" {
+resource "aws_s3_bucket" "vprofile_bucket" {
   bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "vprofile_bucket" {
+  bucket = aws_s3_bucket.vprofile_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "vprofile_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.vprofile_bucket]
+
+  bucket = aws_s3_bucket.vprofile_bucket.id
   acl    = "private"
 }
 
