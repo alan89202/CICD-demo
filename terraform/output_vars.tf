25f870974fce1723a3993c9f32ef2c103e7ca75e
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-08 19:45:53 +0200

    Update output_vars.tf

--- terraform/output_vars.tf
+++ terraform/output_vars.tf
@@ -2,10 +2,4 @@ output "bucket_name" {
   value = var.bucket_name
 }
 
-data "http" "ip" {
-  url = "https://ipinfo.io"
-}
 
-output "my_public_ip" {
-  value = "format("%s/32", jsondecode(data.http.ip.body).ip)"
-}
