4ab065cf09eb7e2b5cff5e9af6e5cc9b12686700
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-08 16:23:58 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,17 @@ variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
 }
+variable "aws_region" {
+  description = "AWS Region"
+  
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  sensitive   = true
+}
