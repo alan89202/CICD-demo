f3b434e2a21848406353352b9ca4644a6883d0a9
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-08 19:27:51 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,41 @@ resource "aws_security_group" "vprofile-ELB-SG" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 }
+resource "aws_security_group" "vprofile-APP-SG" {
+  name        = var.app_sg
+  description = "Security group for tomcat application"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-ELB-SG.id]  
+  }
+}
+resource "aws_security_group" "vprofile-BACKEND-SG" {
+  name        = var.backend_sg
+  description = "Security group for tomcat application"
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+    from_port   = 11211
+    to_port     = 11211
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+      from_port   = 0
+      to_port     = 65535
+      protocol    = "tcp"
+      self        = true  
+    }
+}
