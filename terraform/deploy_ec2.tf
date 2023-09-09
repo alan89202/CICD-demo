8ff664733659b2690c290bcd0bdcba5d6cb09dd1
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-09 23:03:32 +0200

    Update deploy_ec2.tf

--- terraform/deploy_ec2.tf
+++ terraform/deploy_ec2.tf
@@ -24,10 +24,13 @@ resource "aws_instance" "db_instance" {
   }
   user_data = <<EOF
 #!/bin/bash
-sudo yum install wget unzip -y
+sudo yum install tar wget unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
+sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/observabilty_elastic/attach_agents.sh -O /tmp/attach_agents.sh
+sudo chmod +x /tmp/attach_agents.sh
+sudo /tmp/attach_agents.sh "${var.elastic_url}" "${var.elastic_token}" &
 sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/vprofile/userdata/mysql.sh -O /tmp/mysql.sh
 sudo chmod +x /tmp/mysql.sh
 sudo /tmp/mysql.sh
@@ -51,10 +54,13 @@ resource "aws_instance" "mc_instance" {
   }
   user_data = <<EOF
 #!/bin/bash
-sudo yum install wget unzip -y
+sudo yum install tar wget unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
+sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/observabilty_elastic/attach_agents.sh -O /tmp/attach_agents.sh
+sudo chmod +x /tmp/attach_agents.sh
+sudo /tmp/attach_agents.sh "${var.elastic_url}" "${var.elastic_token}" &
 sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/vprofile/userdata/memcache.sh -O /tmp/memcache.sh
 sudo chmod +x /tmp/memcache.sh
 sudo /tmp/memcache.sh
@@ -78,10 +84,13 @@ resource "aws_instance" "rmq_instance" {
   }
   user_data = <<EOF
 #!/bin/bash
-sudo yum install wget unzip -y
+sudo yum install tar wget unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
+sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/observabilty_elastic/attach_agents.sh -O /tmp/attach_agents.sh
+sudo chmod +x /tmp/attach_agents.sh
+sudo /tmp/attach_agents.sh "${var.elastic_url}" "${var.elastic_token}" &
 sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/vprofile/userdata/rabbitmq.sh -O /tmp/rabbitmq.sh
 sudo chmod +x /tmp/rabbitmq.sh
 sudo /tmp/rabbitmq.sh
@@ -107,10 +116,13 @@ resource "aws_instance" "app_instance" {
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install wget awscli -y
+sudo apt install tar wget awscli -y
 sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/scripts/deploy_artifact.sh -O /tmp/deploy_artifact.sh
 sudo chmod +x /tmp/deploy_artifact.sh
 sudo /tmp/deploy_artifact.sh "${aws_s3_bucket.vprofile_bucket.bucket}" "${var.war_file_name}" &
+sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/observabilty_elastic/attach_agents.sh -O /tmp/attach_agents.sh
+sudo chmod +x /tmp/attach_agents.sh
+sudo /tmp/attach_agents.sh "${var.elastic_url}" "${var.elastic_token}" &
 sudo wget https://raw.githubusercontent.com/alan89202/CICD-demo/main/vprofile/userdata/tomcat_ubuntu.sh -O /tmp/tomcat_ubuntu.sh
 sudo chmod +x /tmp/tomcat_ubuntu.sh
 sudo /tmp/tomcat_ubuntu.sh 
