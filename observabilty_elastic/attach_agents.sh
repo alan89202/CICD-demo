488d8158edd00ea33000a4870777bf16e8d22fd5
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-09 23:24:44 +0200

    Update attach_agents.sh

--- observabilty_elastic/attach_agents.sh
+++ observabilty_elastic/attach_agents.sh
@@ -3,6 +3,7 @@ URL=$1
 TOKEN=$2
 
 curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.9.2-linux-x86_64.tar.gz
-tar xzvf elastic-agent-8.9.2-linux-x86_64.tar.gz
-cd elastic-agent-8.9.2-linux-x86_64
+sudo cd /tmp/
+sudo tar xzvf elastic-agent-8.9.2-linux-x86_64.tar.gz
+sudo cd elastic-agent-8.9.2-linux-x86_64
 sudo ./elastic-agent install --url=$URL --enrollment-token=$TOKEN
