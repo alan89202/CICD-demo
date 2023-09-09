078a910eacc0614abbf94e60b5e79413ef96b5ca
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-09 13:43:21 +0200

    Create deploy_sg.tf

---
+++ terraform/deploy_sg.tf
@@ -0,0 +1,80 @@
+//Create security groups used within application
+data "http" "ipinfo" {
+  url = "https://ipinfo.io"
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+resource "aws_security_group" "vprofile-ELB-SG" {
+  name        = var.elb_sg
+  description = "Security group for ELB"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+}
+resource "aws_security_group" "vprofile-APP-SG" {
+  name        = var.app_sg
+  description = "Security group for tomcat application"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-ELB-SG.id]  
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [format("%s/32", jsondecode(data.http.ipinfo.response_body).ip)]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [format("%s/32", jsondecode(data.http.ipinfo.response_body).ip)]
+  }
+}
+resource "aws_security_group" "vprofile-BACKEND-SG" {
+  name        = var.backend_sg
+  description = "Security group for tomcat application"
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+    from_port   = 11211
+    to_port     = 11211
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    security_groups = [aws_security_group.vprofile-APP-SG.id]  
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true  
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [format("%s/32", jsondecode(data.http.ipinfo.response_body).ip)]
+  }
+}
