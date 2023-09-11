1477f58a0f5c423b6428361549f880bfd41d3269
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-09-11 10:43:42 +0200

    Update output_vars.tf

--- terraform/output_vars.tf
+++ terraform/output_vars.tf
@@ -5,17 +5,14 @@ output "bucket_name" {
 locals {
   all_instances = {
     for instance in aws_instance:
-      instance.tags["Name"] => {
-        "public_ip" => instance.public_ip,
-        "os" => instance.tags["OS"]
+      instance.tags["Name"] = {
+        "public_ip" = instance.public_ip,
+        "os" = instance.tags["OS"]
       }
     if instance.tags["Name"] != null && contains(instance.tags["Name"], "vprofile")
   }
 }
 
-
 output "instances" {
   value = local.all_instances
 }
-
-
